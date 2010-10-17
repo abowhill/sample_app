98c62dc2480cc0028c426723c15eb0d10da9e981
abowhill <ajcbowhill@gmail.com>
2010-10-16 21:10:24 -0700

    Layout and routes done

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,4 +21,9 @@ describe "LayoutLinks" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
